ff22ae28d03cdb2f5df2970b33c59ab63a08104a
Rob Miracle <rob@robmiracle.com>
2012-02-23 14:04:56 -0500

    First commit

---
+++ loadsave.lua
@@ -0,0 +1,30 @@
+local json = require("json")
+ 
+function saveTable(t, filename)
+    local path = system.pathForFile( filename, system.DocumentsDirectory)
+    local file = io.open(path, "w")
+    if file then
+        local contents = json.encode(t)
+        file:write( contents )
+        io.close( file )
+        return true
+    else
+        return false
+    end
+end
+ 
+function loadTable(filename)
+    local path = system.pathForFile( filename, system.DocumentsDirectory)
+    local contents = ""
+    local myTable = {}
+    local file = io.open( path, "r" )
+    if file then
+        -- read all contents of file into a string
+        local contents = file:read( "*a" )
+        myTable = json.decode(contents);
+        io.close( file )
+        return myTable
+    end
+    return nil
+end
+
